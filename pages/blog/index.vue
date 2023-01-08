5eb6ca40ba39831c6264da70f1a9e483f1024942
FerminCho <fermin.liljeqvist@gmail.com>
2023-01-08 19:41:15 +0100

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,9 @@
   <main>
     <section v-if="posts" class="w-full max-w-5xl mx-auto">
       <h1 class="title">Blog</h1>
+      <!--
       <posts post-type="blog" :amount="10" />
+    -->
     </section>
   </main>
 </template>
